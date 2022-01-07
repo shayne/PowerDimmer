b1cb70335e1eaa2b1ae0737bd82c823931e7618d
shayne <79330+shayne@users.noreply.github.com>
2022-01-07 11:31:33 -0500

    DPI Multi-monitor Not Being Filled Properly (#2)
    Move to a "dim window" per monitor to solve multi DPI/scaling issues

--- DimWindow.xaml.cs
+++ DimWindow.xaml.cs
@@ -27,12 +27,16 @@ namespace PowerDimmer
             opacityBinding.Mode = BindingMode.OneWay;
             opacityBinding.Source = brightness;
 
-            this.SetBinding(Window.OpacityProperty, opacityBinding);
+            SetBinding(Window.OpacityProperty, opacityBinding);
         }
 
         protected override void OnActivated(EventArgs e)
         {
             base.OnActivated(e);
+
+            // Note: Moving this to the xaml or anywhere else
+            // causes the window to stay on the primary display.
+            WindowState = WindowState.Maximized;
 
             var style = Win32.GetWindowLong(Handle, Win32.GWL_EXSTYLE);
             Win32.SetWindowLong(Handle, Win32.GWL_EXSTYLE, style | Win32.WS_EX_LAYERED | Win32.WS_EX_TRANSPARENT | Win32.WS_EX_TOOLWINDOW | Win32.WS_EX_NOACTIVATE);
